9baebe323396406e0f1e8fb743ecac0a303b43e6
Yvo Weidmann <33548040+yweidmann@users.noreply.github.com>
2018-08-02 17:29:36 +0200

    Adding metadata columns to volume change
    Adding two columns for height capture method and one column for analysis method to the table volume_change.volume_change.
    Creating lookup tables for height capture methods and volume change analysis methods and inserting values.

---
+++ src/INSERT/administration/INSERT_height_capture_method_type.sql
@@ -0,0 +1,28 @@
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 0, 'Not defined or unknown', 'Not defined or unknown.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 1, 'No height captured', 'No height captured. Only 2D data.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 2, 'GPS with elipsoid height correction', 'GPS with elipsoid height correction. Height as LN02.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 3, 'GPS without elipsoid height correction', 'GPS without elipsoid height correction. Height as WGS84.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 4, 'PixelMaps 25k', 'PixelMaps 1:25000');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 5, 'PixelMaps 50k', 'PixelMaps 1:50000');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 6, 'Stereo digitising', 'Stereo digitising with digital Photogrammetry.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 7, 'DHM25', 'DHM25.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 8, 'SwissAlti3D', 'SwissAlti3D.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES ( 9, 'Unknown', 'Unknown.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES (10, 'Miscellaneous digitised contour lines', 'Miscellaneous digitised contour lines.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES (12, 'Stereoplotter', 'Stereo digitising with analog Photogrammetry.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES (13, 'Topographical maps 50k', 'First edition of the topographical maps 1:50000.');
+INSERT INTO administration.height_capture_method_type (pk, short_name, description) 
+	VALUES (14, 'Theodolite or totalstation', 'Theodolite or totalstation.');
---
+++ src/INSERT/volume_change/INSERT_volume_change_analysis_type.sql
@@ -0,0 +1,6 @@
+INSERT INTO volume_change.volume_change_analysis_type (pk, short_name, description) 
+	VALUES ( 0, 'Not defined or unknown', 'Not defined or unknown.');
+INSERT INTO volume_change.volume_change_analysis_type (pk, short_name, description) 
+	VALUES ( 1, 'DSM difference with outlines', 'DSM difference with using glacier outlines.');
+INSERT INTO volume_change.volume_change_analysis_type (pk, short_name, description) 
+	VALUES ( 2, 'DSM difference without outlines', 'DSM difference without using glacier outlines.');
