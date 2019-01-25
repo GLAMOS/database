9931d311a541be5fbae8d6b74bc54d57734153d3
Yvo Weidmann <33548040+yweidmann@users.noreply.github.com>
2019-01-25 16:30:07 +0100

    Only public data delivered to the web portal
    Only data which have no embargo are accessible for the web portal.

--- src/CREATE/length_change/CREATE_web_length_change.sql
+++ src/CREATE/length_change/CREATE_web_length_change.sql
@@ -11,6 +11,7 @@ SELECT
 	lcd.length_change_cumulative                AS variation_cumulative
 FROM length_change.vw_length_change_data AS lcd 
 LEFT JOIN base_data.vw_glacier AS g ON (lcd.pk_glacier = g.pk)
+WHERE lcd.data_embargo_type = 0
 ORDER BY lcd.pk_glacier ASC, lcd.measure_date_start ASC;
 
 GRANT SELECT ON length_change.web_length_change TO glporo;
