61ef7b3c8add3220e2ddc753a482034714a932d4
Elias Hodel <hodel@vaw.baug.ethz.ch>
2019-07-17 11:30:20 +0200

    add cumulative mass balances

--- src/CREATE/mass_balance/WEB_annual_mass_balance_fix_date.sql
+++ src/CREATE/mass_balance/WEB_annual_mass_balance_fix_date.sql
@@ -8,8 +8,10 @@ CREATE OR REPLACE VIEW mass_balance.web_mass_balance_fix_date AS
 		EXTRACT (YEAR FROM mbfd.date_from_annual) AS year_from,
 		EXTRACT (YEAR FROM mbfd.date_to_annual)   AS year_to,
 
-		mbfd.mass_balance_annual                           AS annual_mass_balance
-	FROM mass_balance.vw_mass_balance_fix_date AS mbfd
+		mbfd.mass_balance_annual                           AS annual_mass_balance,
+		mbfd.mass_balance_annual_cumulative                AS annual_mass_balance_cumulative
+	
+	FROM mass_balance.vw_mass_balance_fix_date_cumulative AS mbfd
 	LEFT JOIN base_data.vw_glacier AS g ON (mbfd.pk_glacier = g.pk);
 
 GRANT SELECT ON mass_balance.web_mass_balance TO glporo;
