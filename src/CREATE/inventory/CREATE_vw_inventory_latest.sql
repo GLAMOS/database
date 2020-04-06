15a80e912850184efb1586143c801348c69d6746
Elias Hodel <hodel@vaw.baug.ethz.ch>
2020-04-06 09:04:50 +0200

    changes

--- src/CREATE/inventory/CREATE_vw_inventory_latest.sql
+++ src/CREATE/inventory/CREATE_vw_inventory_latest.sql
@@ -3,17 +3,15 @@ CREATE OR REPLACE VIEW inventory.vw_inventory_latest AS
 		row_number() OVER() AS gid,
 		latest_inventory.*,
 		st_area(st_transform(latest_inventory.geom, 2056))::bigint AS area
+		latest_inventor.year_acquisition
 	FROM (
 		SELECT
 		i.pk_glacier,
 		ST_UNION(i.geom) AS geom,
 		i.sgi_release,
-		i.pk_sgi,
-		i.year_acquisition,
-		i.name_short
 
 		FROM inventory.vw_inventory AS i
 
 		JOIN inventory.vw_inventory_latest_year AS liry ON i.sgi_release = liry.latest_inventory_release_year
 
-		GROUP BY i.pk_glacier, i.sgi_release, i.pk_sgi, i.year_acquisition,i.name_short) AS latest_inventory;
+		GROUP BY i.pk_glacier, i.sgi_release) AS latest_inventory;
