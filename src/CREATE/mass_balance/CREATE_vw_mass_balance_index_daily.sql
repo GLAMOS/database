a7f987168d2f3af4780b7fdf38e43a7604a2c749
Elias Hodel <hodel@vaw.baug.ethz.ch>
2021-07-19 14:17:45 +0200

    add view mass balance index daily

---
+++ src/CREATE/mass_balance/CREATE_vw_mass_balance_index_daily.sql
@@ -0,0 +1,36 @@
+-- View: mass_balance.vw_mass_balance_index_daily
+
+-- DROP VIEW mass_balance.vw_mass_balance_index_daily;
+
+CREATE OR REPLACE VIEW mass_balance.vw_mass_balance_index_daily AS
+SELECT row_number() OVER () AS gid,
+    g.pk_sgi,
+    g.name_full as glacier_name,
+	g.name_short as glacier_short,
+	name as stake_name,
+	date,
+	st_transform(st_setsrid(st_makepoint(latitude::double precision, longitude::double precision), 21781), 4326) AS geom,
+	latitude,
+	longitude,
+	altitude,
+	balance as mass_balance,
+	accumulation,
+	melt,
+	st.name,
+	temperature,
+	precipitation,
+	reference,
+	investgator,
+	creation_date
+	
+
+   FROM mass_balance.index_daily ind
+     LEFT JOIN base_data.vw_glacier g ON ind.fk_glacier = g.pk
+	 LEFT JOIN mass_balance.surface_type st ON ind.fk_surface_type = st.pk;
+
+ALTER TABLE mass_balance.vw_mass_balance_index_daily
+    OWNER TO gladmin;
+
+GRANT ALL ON TABLE mass_balance.vw_mass_balance_index_daily TO gladmin;
+GRANT SELECT ON TABLE mass_balance.vw_mass_balance_index_daily TO glrw;
+GRANT SELECT ON TABLE mass_balance.vw_mass_balance_index_daily TO glro;
